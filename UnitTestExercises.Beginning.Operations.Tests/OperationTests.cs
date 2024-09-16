65864077a8e3c8749f71bfaf44ae484d334ec11a
evren <evren@EVRENAKTAS>
2024-09-17 00:59:31 +0300

    Add project files.

---
+++ UnitTestExercises.Beginning.Operations.Tests/OperationTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestExercises.Beginning.Operations.Tests
+{
+    public class OperationTests
+    {
+        Operations operations = new Operations();
+        [Theory]
+        [InlineData("Counter-Strike Global Offensive", 1500)]
+        [InlineData("Grand Theft Auto 5 (GTA 5)", 1)]
+        [InlineData("Minecraft", 100)]
+        public void Operations_CreateGame_Success(string gameName, decimal price)
+        {
+            var result = operations.CreateGame(gameName, price);
+            Assert.True(result);
+        }
+
+
+        [Fact]
+        public void Operations_CreateGame_NotValidGameName()
+        {
+            string gameName = "";
+            decimal price = 1500;
+            var result = operations.CreateGame(gameName, price);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Operations_CreateGame_NotValidPrice()
+        {
+            string gameName = "Call Of Duty";
+            decimal price = -1;
+            var result = operations.CreateGame(gameName, price);
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData(499)]
+        public void Operation_IsEligible_TotalPurchase_LessThan500_ReturnsFalse(decimal totalPurchase)
+        {
+            var result = operations.IsEligibleForDiscount(totalPurchase);
+            Assert.False(result);
+        }
+
+
+        [Theory]
+        [InlineData(500)]
+        [InlineData(501)]
+        public void Operation_IsEligible_TotalPurchase_GreaterThan500_ResturnsTrue(decimal totalPurchase)
+        {
+            var result = operations.IsEligibleForDiscount(totalPurchase);
+            Assert.True(result);
+        }
+
+
+
+        [Theory]
+        [InlineData(null, "123aBc")]
+        [InlineData("", "123aBc")]
+        public void Operation_ValidateUserRegistration_UserName_NullOrEmpty_ReturnsFalse(string userName, string password)
+        {
+            var result = operations.ValidateUserRegistration(userName, password);
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData("eaktas", "123aB")]
+        public void Operation_ValidateUserRegistration_Password_LessThan6Characters_ReturnsFalse(string userName, string password)
+        {
+            var result = operations.ValidateUserRegistration(userName, password);
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData("eaktas", "123aBc")]
+        public void Operation_ValidateUserRegistration_ValidUserNamePassword_ReturnsTrue(string userName, string password)
+        {
+            var result = operations.ValidateUserRegistration(userName, password);
+            Assert.True(result);
+        }
+    }
+}
---
+++ UnitTestExercises.Beginning.Operations/Operations.cs
@@ -0,0 +1,40 @@
+﻿namespace UnitTestExercises.Beginning.Operations
+{
+    public class Operations
+    {
+        #region Oyun Kayıt, gameName boş değil, price 0 dan küçük değil,
+        public bool CreateGame(string gameName, decimal price)
+        {
+            if (string.IsNullOrEmpty(gameName))
+                return false;
+
+            if (price <= 0)
+                return false;
+
+            return true;
+        }
+        #endregion
+        #region Müşteri İndirim Uygunluğu, Toplam Satın Alma 500 üzeri ise True Değil ise False
+        public bool IsEligibleForDiscount(decimal totalPurchase)
+        {
+            if (totalPurchase >= 500)
+                return true;
+            else
+                return false;
+        }
+        #endregion
+        #region Kullanıcı Kayıt İşlemi, Kullanıcı adı boş olamaz, parola en az 6 karakter ise True Değil ise False
+
+        public bool ValidateUserRegistration(string userName, string password)
+        {
+            if (string.IsNullOrEmpty(userName))
+                return false;
+
+            if (password.Length < 6)
+                return false;
+
+            return true;
+        }
+        #endregion
+    }
+}
